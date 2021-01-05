c1a8564a3440335a438ef308433e7bd3509ac373
vipula_jivani <v1bhalodia@gmail.com>
2021-01-05 18:10:47 +0530

    set top navigator and bottom tabs

--- Screen/DrawerNavigatorRoutes.js
+++ Screen/DrawerNavigatorRoutes.js
@@ -8,18 +8,95 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 // Import Screens
 import HomeScreen from './DrawerScreens/HomeScreen';
 import SettingsScreen from './DrawerScreens/SettingsScreen';
+import Bpage1 from './DrawerScreens/Bpage1';
+import Bpage2 from './DrawerScreens/Bpage2';
 import CustomSidebarMenu from './Components/CustomSidebarMenu';
 import NavigationDrawerHeader from './Components/NavigationDrawerHeader';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createMaterialTopTabNavigator();
+const BottomTab = createBottomTabNavigator();
 
+const TabStack = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      tabBarOptions={{
+        activeTintColor: '#FFFFFF',
+        inactiveTintColor: '#F8F8F8',
+        style: {
+          backgroundColor: '#f4511e',
+        },
+        labelStyle: {
+          textAlign: 'center',
+        },
+        indicatorStyle: {
+          borderBottomColor: '#87B56A',
+          borderBottomWidth: 2,
+        },
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home Screen',
+        }}
+      />
+      <Tab.Screen
+        name="SettingsScreen"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Explore Screen',
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+const BottomTabStack = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      tabBarOptions={{
+        activeTintColor: '#FFFFFF',
+        inactiveTintColor: '#F8F8F8',
+        style: {
+          backgroundColor: '#f4511e',
+        },
+        labelStyle: {
+          textAlign: 'center',
+        },
+        indicatorStyle: {
+          borderBottomColor: '#87B56A',
+          borderBottomWidth: 2,
+        },
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={Bpage1}
+        options={{
+          tabBarLabel: 'Page 1',
+        }}
+      />
+      <Tab.Screen
+        name="SettingsScreen"
+        component={Bpage2}
+        options={{
+          tabBarLabel: 'Page2',
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 const homeScreenStack = ({navigation}) => {
   return (
     <Stack.Navigator initialRouteName="HomeScreen">
       <Stack.Screen
         name="HomeScreen"
-        component={HomeScreen}
+        // component={HomeScreen}
+        component={TabStack}
         options={{
           title: 'Home', //Set Header Title
           headerLeft: () => (
@@ -56,7 +133,8 @@ const settingScreenStack = ({navigation}) => {
       }}>
       <Stack.Screen
         name="SettingsScreen"
-        component={SettingsScreen}
+        component={BottomTabStack}
+        // component={SettingsScreen}
         options={{
           title: 'Settings', //Set Header Title
         }}
---
+++ Screen/DrawerScreens/Bpage1.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const Bpage1 = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 20,
+              textAlign: 'center',
+              marginBottom: 16,
+            }}>
+            Example of Splash, Login and Sign Up in React Native
+            {'\n\n'}
+            This is the page 1
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Bpage1;
