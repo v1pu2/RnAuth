d870bf1dd73506051cb6d7d3794d2930dbd6381e
vipula_jivani <v1bhalodia@gmail.com>
2021-01-13 15:54:34 +0530

    set page 2 with radio buttons demo

--- Screen/DrawerScreens/Bpage2.js
+++ Screen/DrawerScreens/Bpage2.js
@@ -1,28 +1,68 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Button} from 'react-native';
+import {RadioButton} from 'react-native-paper';
 
-const Bpage2 = () => {
+const SubChild = (props) => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1, padding: 16}}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: 'center',
-              marginBottom: 16,
-            }}>
-            Example of Splash, Login and Sign Up in React Native
-            {'\n\n'}
-            This is the page 2
-          </Text>
+    <View>
+      <Button title={props.title} onPress={() => props.onSubmit('subchild')} />
+    </View>
+  );
+};
+
+const Child = (props) => {
+  return (
+    <View>
+      <Button title={props.title} onPress={() => props.onSubmit('child')} />
+    </View>
+  );
+};
+
+const Parent = (props) => {
+  return (
+    <View>
+      <Button title={props.title} onPress={() => props.onSubmit('parent')} />
+    </View>
+  );
+};
+const Bpage2 = (props) => {
+  const [checked, setChecked] = React.useState('parent');
+
+  const onSubmit = (str) => {
+    console.log('submit click n parent***********', str);
+    setChecked(str);
+  };
+  return (
+    <SafeAreaView style={{flex: 1, padding: 20}}>
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <Text>parent</Text>
+          <RadioButton
+            value="parent"
+            status={checked === 'parent' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('parent')}
+          />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text>child</Text>
+          <RadioButton
+            value="child"
+            status={checked === 'child' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('child')}
+          />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text>subChild</Text>
+          <RadioButton
+            value="subchild"
+            status={checked === 'subchild' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('subchild')}
+          />
         </View>
       </View>
+      <Parent onSubmit={onSubmit} title="parent" />
+      <Child title="child" onSubmit={onSubmit} />
+      <SubChild title="sub child" onSubmit={onSubmit} />
     </SafeAreaView>
   );
 };
